d5e2064b7d6bdff7f7ed2d0ec4568625f15b302a
L <jlsgkid704@gmail.com>
2017-06-20 14:39:59 +0900

    Update LightTransform.cs

--- Scripts/Menu/LightTransform.cs
+++ Scripts/Menu/LightTransform.cs
@@ -7,14 +7,10 @@ public class LightTransform : MonoBehaviour {
 	public GameObject transLight;
 	//public bool isIn=false;
 	public float distance = 0;	
-	public float m_liveTime = 3.0f;
+	public float m_liveTime = 4.0f;
 	public Transform mozhang;
-	public Transform orig;
-
-	void Awake(){
-		orig = transLight.transform;
-	}
-
+	//public Transform orig;
+	
 	private IEnumerator LightTransGo(){
 		while(m_liveTime >= 0){
 			transLight.gameObject.transform.Translate(new Vector3(0, 4 * Time.deltaTime,0 ));
@@ -23,10 +19,10 @@ public class LightTransform : MonoBehaviour {
 		}
 
 	}
+	
 	private IEnumerator LightTransBack(){
 		float dis = Vector3.Distance(transLight.gameObject.transform.position, mozhang.position);
 		while(dis >5){
-			
 			transLight.gameObject.transform.rotation = Quaternion.Slerp(transLight.gameObject.transform.rotation, 
 				Quaternion.LookRotation(mozhang.position-transLight.gameObject.transform.position), 10 * Time.deltaTime);
 			//transLight.gameObject.transform.position += transLight.gameObject.transform.forward * 10 * Time.deltaTime;
@@ -40,22 +36,14 @@ public class LightTransform : MonoBehaviour {
 	public void StartBlink(){
 		transLight.SetActive (true);
 		StartCoroutine(LightTransGo ());
-
-		Invoke ("DispLight", 3.0f);
+		//Invoke ("DispLight", 4.0f);
+		DispLight();
 	}
 
 	private void DispLight(){
 		transLight.gameObject.transform.localPosition = Vector3.zero;
-		m_liveTime = 3.0f;
+		m_liveTime = 4.0f;
 		transLight.SetActive (false);
 	}
-//	void Update(){
-//		if(Input.GetKeyDown(KeyCode.C)){
-//			StartCoroutine(LightTransGo ());
-//			if(m_liveTime <= 0 ){
-//				//StartCoroutine(LightTransBack ());
-//			}
-//		}
-//
-//	}
+
 }
